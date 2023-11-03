6da8a9b9b40fcbe61a3dff460c8de7a4e04a849e
Jayson Llanes <144930437+jayjay009@users.noreply.github.com>
2023-11-03 17:03:52 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+document.getElementById('next').onclick = function(){
+  let lists = document.querySelectorAll('.item');
+  document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+  let lists = document.querySelectorAll('.item');
+  document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
